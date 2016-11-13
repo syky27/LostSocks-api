d8ea74f734f68e738f8f3b381538fe689b2fd7af
Tomas Sykora, jr <jump@syky.cz>
2016-11-13 21:37:59 +0100

    BROKEN: ENV not working in POSQL config

--- Package.swift
+++ Package.swift
@@ -3,10 +3,8 @@ import PackageDescription
 let package = Package(
 	name: "LostSocks",
 	dependencies: [
-		.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
-//		.Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1),
-		.Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0),
-		.Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 0)
+    .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
+    .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 0),
 
 	],
 	exclude: [
